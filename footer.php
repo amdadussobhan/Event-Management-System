5746892ca8218fbfc2e9b1ab400b318d7232a502
AMDADUS SOBHAN <amdadulislamcl@gmail.com>
2025-01-22 23:06:35 +0600

    added signup

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 
     <footer class="bg-light text-center text-lg-start mt-5">
         <div class="text-center p-3 bg-dark text-white">
-            &copy; <?php echo date("Y"); ?> Event Management System. All rights reserved.
+            Copyright &copy; <?php echo date("Y"); ?> AMDADUS SOBHAN. All rights reserved.
         </div>
     </footer>
     
--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-success">
+<nav class="navbar navbar-expand-lg bg-info">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Event Management</a>
+        <a class="navbar-brand" href="/ems/"><img src="Logo.png" style="height: 44px;"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -12,11 +12,19 @@
                 <li class="nav-item">
                     <a class="nav-link" href="events.php">Events</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="admin_dashboard.php">Admin</a>
+            </ul>
+            
+            <h2 class="ms-auto">Event Management System</h2>
+            
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item d-flex ">
+                    <a class="nav-link" href="login.php">Login</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="logout.php">Logout</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="register.php">Sign up</a>
                 </li>
             </ul>
         </div>
